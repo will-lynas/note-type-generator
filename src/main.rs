90d2675c1648faf818d087cabbccc9810eef1b9d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-29 20:46:31 +0100

    Add custom css example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use genanki_rs::{Deck, Error, Field, Model, Note, Template};
 
 fn main() -> Result<(), Box<Error>> {
+    let custom_css = ".card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n}\n";
+
     let my_model = Model::new(
         1607392319,
         "Simple Model",
@@ -11,13 +13,17 @@ fn main() -> Result<(), Box<Error>> {
         vec![Template::new("Card 1")
             .qfmt("{{Question}}")
             .afmt(r#"{{FrontSide}}<hr id="answer">{{Answer}}"#)],
-    );
+    )
+    .css(custom_css);
+
     let my_note = Note::new(my_model, vec!["Capital of Argentina", "Buenos Aires"])?;
+
     let mut my_deck = Deck::new(
         2059400110,
         "Country Capitals",
         "Deck for studying country capitals",
     );
+
     my_deck.add_note(my_note);
     my_deck.write_to_file("output.apkg")?;
     Ok(())
