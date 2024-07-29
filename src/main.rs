48c0236bf5d15ac2b8666ac2bebed811633bdb57
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-29 22:40:46 +0100

    Add error handling for css file

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,19 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+const CSS_FILE_PATH: &str = "input/style.css";
+
 fn main() -> Result<(), Box<dyn Error>> {
-    // TODO: handle the case when file doesn't exist
-    let css_file = File::open("input/style.css")?;
-    let mut buf_reader = BufReader::new(css_file);
     let mut custom_css = String::new();
-    // TODO: handle error reading string
-    buf_reader.read_to_string(&mut custom_css)?;
+
+    if let Ok(css_file) = File::open(CSS_FILE_PATH) {
+        let mut buf_reader = BufReader::new(css_file);
+
+        if let Err(e) = buf_reader.read_to_string(&mut custom_css) {
+            eprintln!("Error reading file {}: {}", CSS_FILE_PATH, e);
+            return Err(Box::new(e));
+        }
+    }
 
     let my_model = Model::new(
         1607392319,
