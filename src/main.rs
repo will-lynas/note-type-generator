2e46ff202409128d3de5c5a4f0d5ef24a5c7bf76
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-07-29 22:34:23 +0100

    Read css from file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use genanki_rs::{Deck, Field, Model, Note, Template};
 use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let custom_css = ".card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n}\n";
+    // TODO: handle the case when file doesn't exist
+    let css_file = File::open("input/style.css")?;
+    let mut buf_reader = BufReader::new(css_file);
+    let mut custom_css = String::new();
+    // TODO: handle error reading string
+    buf_reader.read_to_string(&mut custom_css)?;
 
     let my_model = Model::new(
         1607392319,
