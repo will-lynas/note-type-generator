8470251d3878d18e089d7fab29912ac07034f7f4
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-14 00:33:34 +0200

    Move template generation to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod args;
 mod config;
 mod files;
+mod templates;
 
 use std::collections::hash_map::DefaultHasher;
 use std::error::Error;
 use std::hash::{Hash, Hasher};
 
-use genanki_rs::{Deck, Field, Model, Note, Template};
-use regex::Regex;
+use genanki_rs::{Deck, Field, Model, Note};
 
 use files::Files;
 
@@ -25,43 +25,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let files = Files::load(args.template, args.css, args.config);
     let config = config::get(files.config);
 
-    for template_config in &config.templates {
-        if !config.fields.contains(&template_config.question_field) {
-            panic!(
-                "Question field '{}' is not in fields",
-                template_config.question_field
-            );
-        }
-
-        for front_field in &template_config.front_fields {
-            if !config.fields.contains(front_field) {
-                panic!("Front field '{}' is not in fields", front_field);
-            }
-        }
-    }
-
-    let field_pattern = Regex::new(r"\{\{([^\}]+)\}\}").unwrap();
-
-    let all_fields_in_template: Vec<String> = field_pattern
-        .captures_iter(&files.template)
-        .map(|cap| cap[1].to_string())
-        .collect();
-
-    for field in &config.fields {
-        if !all_fields_in_template.contains(field) {
-            panic!("Field '{}' in config is not found in the template", field);
-        }
-    }
-
-    for field in &all_fields_in_template {
-        if !config.fields.contains(field) {
-            panic!(
-                "Field '{}' in template is not found in the config fields",
-                field
-            );
-        }
-    }
-
     let my_model = Model::new(
         hash_string_to_i64(&config.note_type_name),
         &config.note_type_name,
@@ -70,41 +33,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             .iter()
             .map(|s| Field::new(&s.clone()).font("Arial"))
             .collect(),
-        config
-            .templates
-            .into_iter()
-            .map(|template_config| {
-                let mut qfmt = files.template.clone();
-                qfmt = qfmt.replace(
-                    &format!("{{{{{}}}}}", template_config.question_field),
-                    r#"<span class="cloze">?</span>"#,
-                );
-
-                let mut afmt = files.template.clone();
-                afmt = afmt.replace(
-                    &format!("{{{{{}}}}}", template_config.question_field),
-                    &format!(
-                        r#"<span class="cloze">{{{{{}}}}}</span>"#,
-                        template_config.question_field
-                    ),
-                );
-
-                for field in &config.fields {
-                    if !template_config.front_fields.contains(field) {
-                        qfmt = qfmt.replace(&format!("{{{{{}}}}}", field), "");
-                    }
-                }
-
-                let qfmt = format!(
-                    "{{{{#{}}}}}\n\n{}\n\n{{{{/{}}}}}",
-                    template_config.question_field, qfmt, template_config.question_field
-                );
-
-                Template::new(&template_config.question_field)
-                    .qfmt(&qfmt)
-                    .afmt(&afmt)
-            })
-            .collect(),
+        templates::create(config.templates, config.fields.clone(), files.template),
     )
     .css(files.css);
 
---
+++ src/templates.rs
@@ -0,0 +1,82 @@
+use genanki_rs::Template;
+use regex::Regex;
+
+use crate::config::TemplateConfig;
+
+pub fn create(
+    template_configs: Vec<TemplateConfig>,
+    fields: Vec<String>,
+    template: String,
+) -> Vec<Template> {
+    for template_config in &template_configs {
+        if !fields.contains(&template_config.question_field) {
+            panic!(
+                "Question field '{}' is not in fields",
+                template_config.question_field
+            );
+        }
+
+        for front_field in &template_config.front_fields {
+            if !fields.contains(front_field) {
+                panic!("Front field '{}' is not in fields", front_field);
+            }
+        }
+    }
+
+    let field_pattern = Regex::new(r"\{\{([^\}]+)\}\}").unwrap();
+
+    let all_fields_in_template: Vec<String> = field_pattern
+        .captures_iter(&template)
+        .map(|cap| cap[1].to_string())
+        .collect();
+
+    for field in &fields {
+        if !all_fields_in_template.contains(field) {
+            panic!("Field '{}' in config is not found in the template", field);
+        }
+    }
+
+    for field in &all_fields_in_template {
+        if !fields.contains(field) {
+            panic!(
+                "Field '{}' in template is not found in the config fields",
+                field
+            );
+        }
+    }
+
+    template_configs
+        .into_iter()
+        .map(|template_config| {
+            let mut qfmt = template.clone();
+            qfmt = qfmt.replace(
+                &format!("{{{{{}}}}}", template_config.question_field),
+                r#"<span class="cloze">?</span>"#,
+            );
+
+            let mut afmt = template.clone();
+            afmt = afmt.replace(
+                &format!("{{{{{}}}}}", template_config.question_field),
+                &format!(
+                    r#"<span class="cloze">{{{{{}}}}}</span>"#,
+                    template_config.question_field
+                ),
+            );
+
+            for field in &fields {
+                if !template_config.front_fields.contains(field) {
+                    qfmt = qfmt.replace(&format!("{{{{{}}}}}", field), "");
+                }
+            }
+
+            let qfmt = format!(
+                "{{{{#{}}}}}\n\n{}\n\n{{{{/{}}}}}",
+                template_config.question_field, qfmt, template_config.question_field
+            );
+
+            Template::new(&template_config.question_field)
+                .qfmt(&qfmt)
+                .afmt(&afmt)
+        })
+        .collect()
+}
