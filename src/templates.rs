adc45daf8c62df2e789970c8264389b5dd1095da
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-14 15:37:51 +0200

    Add first positive test

--- src/templates.rs
+++ src/templates.rs
@@ -3,6 +3,8 @@ use regex::Regex;
 
 use crate::config::TemplateConfig;
 
+// TODO: Only derive these during tests
+#[derive(Debug, PartialEq)]
 struct PreTemplate {
     name: String,
     qfmt: String,
@@ -107,4 +109,75 @@ pub fn create(
                 .afmt(&template.afmt)
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    impl PreTemplate {
+        fn new_from_str(name: &str, qfmt: &str, afmt: &str) -> Self {
+            Self {
+                name: name.to_string(),
+                qfmt: qfmt.to_string(),
+                afmt: afmt.to_string(),
+            }
+        }
+    }
+
+    #[test]
+    fn create_template_success() {
+        let template_configs = vec![
+            TemplateConfig {
+                question_field: String::from("Field1"),
+                front_fields: vec![String::from("Field2")],
+            },
+            TemplateConfig {
+                question_field: String::from("Field3"),
+                front_fields: vec![String::from("Field1"), String::from("Field2")],
+            },
+        ];
+
+        let fields = vec![
+            String::from("Field1"),
+            String::from("Field2"),
+            String::from("Field3"),
+        ];
+
+        let template = String::from("{{Field1}} | {{Field2}} | {{Field3}}");
+
+        let templates = pre_create(template_configs, fields, template);
+
+        assert_eq!(templates.len(), 2);
+
+        let expected_templates: Vec<PreTemplate> = vec![
+            PreTemplate::new_from_str(
+                "Field1",
+                indoc! {r#"
+                {{#Field1}}
+
+                <span class="cloze">?</span> | {{Field2}} | 
+
+                {{/Field1}}
+                "#},
+                indoc! {r#"
+                <span class="cloze">{{Field1}}</span> | {{Field2}} | {{Field3}}"#},
+            ),
+            PreTemplate::new_from_str(
+                "Field3",
+                indoc! {r#"
+                {{#Field3}}
+
+                {{Field1}} | {{Field2}} | <span class="cloze">?</span>
+
+                {{/Field3}}
+                "#},
+                indoc! {r#"
+                {{Field1}} | {{Field2}} | <span class="cloze">{{Field3}}</span>"#},
+            ),
+        ];
+
+        assert_eq!(templates, expected_templates);
+    }
 }
