64be06a22d62b6ea2a288fb922c66124a38a0abf
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-14 20:24:40 +0200

    Add test for template error display

--- src/template_error.rs
+++ src/template_error.rs
@@ -36,3 +36,37 @@ impl fmt::Display for TemplateError {
 }
 
 impl Error for TemplateError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn template_errors() {
+        assert_eq!(
+            format!(
+                "{}",
+                TemplateError::QuestionFieldError("FieldX".to_string())
+            ),
+            "Question field 'FieldX' is not in fields"
+        );
+        assert_eq!(
+            format!("{}", TemplateError::FrontFieldError("FieldX".to_string())),
+            "Front field 'FieldX' is not in fields"
+        );
+        assert_eq!(
+            format!(
+                "{}",
+                TemplateError::TemplateFieldNotInFields("FieldX".to_string())
+            ),
+            "Field 'FieldX' in template is not found in the config fields"
+        );
+        assert_eq!(
+            format!(
+                "{}",
+                TemplateError::FieldNotInTemplate("FieldX".to_string())
+            ),
+            "Field 'FieldX' in config is not found in the template"
+        );
+    }
+}
