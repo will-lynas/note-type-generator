bc225f4704b940549b22da8b55854ff838de8016
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-08-18 11:38:26 +0200

    Add field_not_in_template test

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -73,3 +73,37 @@ fn template_not_in_fields() {
         .stdout("")
         .stderr(expected_stderr);
 }
+
+#[test]
+fn field_not_in_template() {
+    let css_file = NamedTempFile::new().unwrap();
+    let template_file = NamedTempFile::new().unwrap();
+    let mut config_file = NamedTempFile::new().unwrap();
+
+    config_file
+        .write_all(indoc! {br#"
+            fields = [
+            "does_not_exist",
+            ]
+        "#})
+        .unwrap();
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+
+    cmd.arg("--css")
+        .arg(css_file.path())
+        .arg("--template")
+        .arg(template_file.path())
+        .arg("--config")
+        .arg(config_file.path());
+
+    let expected_stderr = indoc! {r#"
+        Error: Field 'does_not_exist' in config is not found in the template
+        "#};
+
+    cmd.assert()
+        .failure()
+        .code(1)
+        .stdout("")
+        .stderr(expected_stderr);
+}
